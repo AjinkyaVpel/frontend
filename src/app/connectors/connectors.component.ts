316d2d608053c36e3466e74c072805ac869bff73
Pratik Pingale <117061982+PratikVpel@users.noreply.github.com>
2023-05-17 18:45:25 +0530

    Revert "Ajinkya"

--- src/app/connectors/connectors.component.ts
+++ src/app/connectors/connectors.component.ts
@@ -17,6 +17,7 @@ export class ConnectorsComponent {
   stationId: any;
   chargerId: any;
   openForm:boolean = false;
+  connctorId:any
   displayedColumns: string[] = [ 'connectorNumber', 'connectorType','connectorSocket','connectorStatus','connectorOutputPower', 'menu'];
   dataSource!: MatTableDataSource<any>;
 
