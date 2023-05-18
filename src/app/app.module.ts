8dd2d84bd00eabbc1bf0aa6fd6fca3c863d4bd75
Virtuososoft <ajinkya.dahale@virtuososofttech.com>
2023-05-18 10:05:12 +0530

    minor bug fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,8 +53,6 @@ import { AddConnectorComponent } from './connectors/add-connector/add-connector.
 import { MatIconModule } from '@angular/material/icon';
 import { ControlAccessComponent } from './manageStation/controlAccess/control-access.component';
 import { SearchPipe } from './chargers/search.pipe';
-import * as _ from "lodash";
-
 
 @NgModule({
   declarations: [
